1b97d57a5309ea29ceb269b292f53a85d1bb544a
AlessandroPatti <ale812@yahoo.it>
2018-01-18 22:10:18 +0100

    Update database

--- Vue/vue-main.js
+++ Vue/vue-main.js
@@ -1,17 +1,8 @@
 let vue_one
 window.addEventListener('load', function () {
     vue_one = new Vue({
-        el: '#product-list',
+        el: '#vue',
     });
     loadDB('database.json')
 });
 
-function loadDB(database){
-    //alert("Loading DB")
-    Plotly.d3.json(database, function(e,data){
-        for(var i=0; i<data.products.length; i++) {
-            p=data.products[i]
-            vue_one.$refs.ref_product_list.addProduct(parseInt(p.id), p.name, parseFloat(p.price), parseFloat(p.rating), parseFloat(p.trend), p.vendor, parseFloat(p.low_price), p.link, p.img_source);
-        }
-    })
-}
