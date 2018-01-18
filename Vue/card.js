02f697695145e7ee09c2eca44b3ffe23e484324b
Giorgio <gavalle94@gmail.com>
2018-01-18 15:17:11 +0100

    Second CSS draft for Vue cards

--- Vue/card.js
+++ Vue/card.js
@@ -1,6 +1,17 @@
+/*
+TEMPLATE FOR THE CARDS
+The card itself is the "vue-card-main-container" element.
+According to the screen size, the "title" can be on top or on the bottom of the card.
+The "track" button is always at bottom.
+Other information and the product image are represented on a single row, inside the element with class "vue-card-top-container"
+
+ */
+
+
 Vue.component('card', {
     template: `
     <div class="vue-card-main-container">
+        <p class="title-box w3-hide-medium w3-hide-large" v-text="title"></p>
         <div class="vue-card-top-container">
             <div class="vue-card-image-box">
                 <img v-if="img_src" class='vue-card-product-image' :src="img_src">
@@ -15,12 +26,13 @@ Vue.component('card', {
                 </div>
             </div>
         </div>
-        <div class="vue-card-bottom-bar w3-hover-teal search-result">
-            <div class="title-box">
-                <span v-text="title"></span>
-            </div>
+        <p class="title-box w3-hide-small">
+            <span v-text="title"></span>
             <button><i class="fa fa-plus"></i> Track</button>
-        </div>
+        </p>
+        <button class="w3-mobile w3-hide-medium w3-hide-large">
+            <i class="fa fa-plus"></i> Track
+        </button>
     </div>
     `,
     name: "card",
