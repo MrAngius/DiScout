498696dc921d888b20e9b54e5ca8d8203be09220
AlessandroPatti <ale812@yahoo.it>
2018-01-19 21:33:03 +0100

    Using sessionStore for product database

--- Vue/vue-main.js
+++ Vue/vue-main.js
@@ -99,16 +99,24 @@ window.addEventListener('load', function () {
 
 function loadDB(database){
     //alert("Loading DB")
-    Plotly.d3.json(database, function(e,data) {
+    if(window.sessionStorage.getItem('database')==undefined) {
+        alert("Not found")
+        Plotly.d3.json(database, function (e, data) {
+            vue.cards = data.products
+            /* Following code is used to insert categories dynamically, not working atm */
+            /*
+            for (var i = 0; i < data.products.length; i++) {
+                var cat = data.products[i].category
+                vue.categories[cat] = true
+            }
+            */
+            console.log(JSON.stringify(data.products))
+            window.sessionStorage.setItem('database', JSON.stringify(data.products))
+        })
+    }
+    else {
+        alert("Found")
+        vue.cards = JSON.parse(window.sessionStorage.getItem('database'))
 
-        vue.cards = data.products
-        /* Following code is used to insert categories dynamically, not working atm */
-        /*
-        for (var i = 0; i < data.products.length; i++) {
-            var cat = data.products[i].category
-            vue.categories[cat] = true
-        }
-        */
-
-    })
+    }
 }
