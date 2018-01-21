9648877d8ee87d4c436e63b9f80249485b7e220a
Giorgio <gavalle94@gmail.com>
2018-01-21 19:01:11 +0100

    Price appears again. Changed filter buttons color

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,6 @@
 function loadDB(database){
     alert("Loading DB");
-    if(window.sessionStorage.getItem('database')===undefined) {
+    if(window.sessionStorage.getItem('database')===null) {
         alert("Not found");
         Plotly.d3.json(database, function (e, data) {
 
