8cfd36a66ae8393eb1555cfab8fe25c2905b1804
AlessandroPatti <ale812@yahoo.it>
2018-01-21 21:55:36 +0100

    Basic structure created

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,31 @@
 <template>
-  <div id="app">
-
-  </div>
+  <main>
+    <vue-header></vue-header>
+    <vue-navbar></vue-navbar>
+    <component v-bind:is="whichpage"></component>
+    <vue-footer></vue-footer>
+  </main>
 </template>
 
 <script>
+  import Header from './components/Header'
+  import NavBar from './components/NavBar'
+  import Footer from './components/Footer'
+  import SearchPage from './components/SearchPage'
+  import GraphPage from './components/GraphPage'
 export default {
   name: 'App',
   components: {
-
+    'vue-header': Header,
+    'vue-navbar': NavBar,
+    'vue-footer': Footer,
+    'vue-searchpage': SearchPage,
+    'vue-graphpage': GraphPage
+  },
+  data() {
+    return{
+      whichpage: 'vue-searchpage'
+    }
   }
 }
 </script>
---
+++ src/components/GraphPage.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "graph-page"
+    }
+</script>
+
+<style scoped>
+
+</style>
